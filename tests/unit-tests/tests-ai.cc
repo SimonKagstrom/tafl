d7ac069978b3a19458a7ef9e134f352068b01483
Simon Kagstrom <simon.kagstrom@gmail.com>
2012-08-24 21:23:02 +0200

    unit test: Add test case for winning moves

--- tests/unit-tests/tests-ai.cc
+++ tests/unit-tests/tests-ai.cc
@@ -63,5 +63,68 @@ TESTSUITE(ai)
 
 		// Better for white (white is more mobile)
 		ASSERT_LT(three, 0);
+
+		delete ai;
+	}
+
+	TEST(win, BoardFixture)
+	{
+		const std::string whiteWinStart =
+				". o      "
+				"  .      "
+				"   o     "
+				"  .      "
+				"  o K    "
+				"    o    "
+				"         "
+				"         "
+				"         ";
+		const std::string whiteWinEnd =
+				". o      "
+				"  .      "
+				"   o     "
+				"  .      "
+				"  o     K"
+				"    o    "
+				"         "
+				"         "
+				"         ";
+
+		IBoard *p;
+		IBoard::Move move;
+		bool res;
+		double eval;
+		Ai *ai = (Ai *)IAi::createAi();
+
+		p = IBoard::fromString(toBoardString(whiteWinStart, IBoard::WHITE));
+		ASSERT_TRUE(p);
+		ASSERT_EQ(p->getWinner(), IBoard::BOTH);
+		ASSERT_EQ(p->getTurn(), IBoard::WHITE);
+
+
+		res = constructMove(whiteWinStart, whiteWinEnd, move);
+		ASSERT_TRUE(res);
+
+
+		// Board two should be better (positive) for black
+		eval = ai->evaluate(*p);
+		ASSERT_TRUE(abs(eval) < ai->m_configuration[VICTORY]);
+
+		res = p->doMove(move);
+		ASSERT_TRUE(res);
+
+		// Stupid move by black
+		IBoard::Move bm(IBoard::Point(0,0), IBoard::Point(0, 1));
+		res = p->doMove(bm);
+		ASSERT_TRUE(res);
+
+
+		// White wins
+		ASSERT_EQ(p->getWinner(), IBoard::WHITE);
+
+		eval = ai->evaluate(*p);
+		ASSERT_TRUE(eval == -ai->m_configuration[VICTORY]);
+
+		delete ai;
 	}
 }

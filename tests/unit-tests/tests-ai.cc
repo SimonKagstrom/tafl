f71bda5cd3c84e553f61242e1eb7b334c2f91fc8
Simon Kagstrom <simon.kagstrom@gmail.com>
2012-08-24 20:42:33 +0200

    unit test: Add test for defender-dominated board
    This test now pass.

--- tests/unit-tests/tests-ai.cc
+++ tests/unit-tests/tests-ai.cc
@@ -32,19 +32,36 @@ TESTSUITE(ai)
 				"    .    "
 				"   ...   ";
 
+		const std::string boardThreeStr =
+				"         "
+				"         "
+				"         "
+				"o        "
+				"..  k    "
+				".   o    "
+				".o       "
+				".        "
+				".        ";
 
-		int one, two;
+
+		double one, two, three;
 		IBoard *boardOne = IBoard::fromString(toBoardString(boardOneStr));
 		IBoard *boardTwo = IBoard::fromString(toBoardString(boardTwoStr));
+		IBoard *boardThree = IBoard::fromString(toBoardString(boardThreeStr));
 		ASSERT_TRUE(boardOne);
 		ASSERT_TRUE(boardTwo);
+		ASSERT_TRUE(boardThree);
 
 		IAi *ai = IAi::createAi();
 
 		// Board two should be better (positive) for black
 		one = ai->evaluate(*boardOne);
 		two = ai->evaluate(*boardTwo);
+		three = ai->evaluate(*boardThree);
 
 		ASSERT_GT(two, one);
+
+		// Better for white (white is more mobile)
+		ASSERT_LT(three, 0);
 	}
 }
